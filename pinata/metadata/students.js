4ec7f2b01e7ff8d2aa01bbec06b6684af10be274
Philipp Erker <Philipp.erker@protonmail.com>
2022-05-21 16:52:49 +0200

    add metadata/students.js (pinata)

---
+++ pinata/metadata/students.js
@@ -0,0 +1,119 @@
+const alex = {
+  name: 'Alex',
+  external_url: 'https://github.com/AlexF090',
+  capstone_project: 'GenialTanken',
+  github_capstone_project: 'https://github.com/D3nnis249/GenialTanken-Capstone',
+  description:
+    'Capstone Project GenialTanken used techniques: React, React Router, React leaflet, React leaflet markercluster, React Testing Library, Jest, Storybook, Styled Components, TankenTanken API',
+  image_path: './images/Alex.png',
+};
+
+const antje = {
+  name: 'Antje',
+  external_url: 'https://github.com/PulexIrritans',
+  capstone_project: 'HEELHOOK',
+  github_capstone_project: 'https://github.com/PulexIrritans/Heelhook-Capstone',
+  description:
+    'Capstone Project HEELHOOK used techniques: React, Node.js, MongoDB Atlas, Express, Ant-Design Charts, Styled Components, Storybook, Jest',
+  image_path: './images/Antje.png',
+};
+
+const daniele = {
+  name: 'Daniele',
+  external_url: 'https://github.com/DanCesca',
+  capstone_project: 'World of Coffee',
+  github_capstone_project: 'https://github.com/DanCesca/capstone-worldofcoffee',
+  description:
+    'Capstone Project World of Coffee used techniques: React, React Router, React Toastify, React Testing Library, Jest, Styled Components',
+  image_path: './images/Daniele.png',
+};
+
+const dennis = {
+  name: 'Dennis',
+  external_url: 'https://github.com/d3nnis249',
+  capstone_project: 'GenialTanken',
+  github_capstone_project: 'https://github.com/D3nnis249/GenialTanken-Capstone',
+  description:
+    'Capstone Project GenialTanken used techniques: React, React Router, React leaflet, React leaflet markercluster, React Testing Library, Jest, Storybook, Styled Components, TankenTanken API',
+  image_path: './images/Dennis.png',
+};
+
+const hamad = {
+  name: 'Hamad',
+  external_url: 'https://github.com/hamedbahrami479',
+  capstone_project: 'GIB Give It Back',
+  github_capstone_project: 'https://github.com/hamedbahrami479/CapStone',
+  description:
+    'Capstone Project GIB Give It Back used techniques: React, REACT Testing Library, Jest, Styled Components',
+  image_path: './images/Hamad.png',
+};
+
+const nima = {
+  name: 'Nima',
+  external_url: 'https://github.com/NimaHBB',
+  capstone_project: 'Nitto Shop',
+  github_capstone_project: 'https://github.com/NimaHBB/capstone-project',
+  description:
+    'Capstone Project Nitto Shop used techniques: React, Express, MongoDB, Node.js, Styled Components, React Router',
+  image_path: './images/Nima.png',
+};
+
+const momchil = {
+  name: 'Momchil',
+  external_url: 'https://github.com/momcil',
+  capstone_project: 'pv-flow',
+  github_capstone_project: 'https://github.com/Momcil/capstone-pvflow',
+  description:
+    'Capstone Project Pv-Flow used techniques: React, Express, MongoDB Atlas, Node.js, Styled Components, React Router',
+  image_path: './images/Momchil.png',
+};
+
+const monique = {
+  name: 'Monique',
+  external_url: 'https://github.com/Moni73',
+  capstone_project: 'KunstStuebchen',
+  github_capstone_project: 'https://github.com/Moni73/KunstStuebchen-Capstone',
+  description:
+    'Capstone Project KunstStuebchen used techniques: React, React Router, CSS',
+  image_path: './images/Monique.png',
+};
+
+const philipp = {
+  name: 'Philipp',
+  external_url: 'https://github.com/philipperker',
+  capstone_project: 'Fratello',
+  github_capstone_project: 'https://github.com/philipperker/capstone-project',
+  description:
+    'Capstone Project Fratello used techniques: React, Node.js, Styled Components, React Router, alchemyapi.io, pinata.cloud, Ipfs, Ethereum, Solidity',
+  image_path: './images/Philipp.png',
+};
+
+const ploy = {
+  name: 'Ploy',
+  external_url: 'https://github.com/Ploysth',
+  capstone_project: 'myFlynet',
+  github_capstone_project: 'https://github.com/Ploysth/capstone_project',
+  description:
+    'Capstone Project myFlynet used techniques: React, Styled Components, React Router, Figma',
+  image_path: './images/Ploy.png',
+};
+
+const sanae = {
+  name: 'Sanae',
+  external_url: 'https://github.com/Sanae-G',
+  capstone_project: 'TravelCatcher',
+  github_capstone_project: 'https://github.com/Sanae-G/CapstoneProject',
+  description:
+    'Capstone Project TravelCatcher used techniques: React, Node.js, Cloudinary, MongoDB, Mongoose, Styled Components, React Testing Library, Express, Bcryptjs, Jsonwebtoken, Validator, React Router ',
+  image_path: './images/Sanae.png',
+};
+
+const zach = {
+  name: 'Zach',
+  external_url: 'https://github.com/zengqiangHe',
+  capstone_project: 'BockWurst',
+  github_capstone_project: 'https://github.com/zengqiangHe/capstone-project',
+  description:
+    'Capstone Project BockWurst used techniques: React, Styled Components, React Router, LocalStorage, React Testing Library, MongoDB, Node.js, Express, Jest, Mongoose',
+  image_path: './images/Zach.png',
+};
